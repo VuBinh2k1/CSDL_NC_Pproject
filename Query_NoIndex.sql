18a3624aa8e8576c6201cb0b9117683d7db46597
Hồng Quân <hongquan4242@gmail.com>
2021-10-27 16:13:30 +0700

    Query 1,2 (No Index)

---
+++ Query_NoIndex.sql
@@ -0,0 +1,11 @@
+﻿USE [QLHoaDon]
+GO
+-- 1. Danh sách các hoá đơn lập trong năm 2020
+SELECT *
+FROM HoaDon
+WHERE YEAR(NgayLap) = '2020'
+
+-- 2. Danh sách các khách hàng ở TPHCM
+SELECT *
+FROM KhachHang
+WHERE TPho = N'Hồ Chí Minh'
